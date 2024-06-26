337a69ad5c72776f7d76009436a17b603ffc7fe8
Leonardo Brandão <leonardobrandaoamarante@gmail.com>
2024-06-26 06:45:11 -0400

    cors origin

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.post('/user', async (req, res) => {
 app.get('/rank', async (req, res) => {
   console.log('GET /rank');
   try {
-    const users = await UserModel.find().sort({ score: 1 });
+    const users = await UserModel.find().sort({ score: -1 });
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: "Error getting users" });
